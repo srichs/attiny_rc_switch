9532f7d1abc52de8641196c6a339e1954fd4c550
srichs <srichs@pm.me>
2025-05-18 02:15:58 -0600

    added pcb, changed code & readme

--- README.md
+++ README.md
@@ -1,24 +1,41 @@
-# attiny_rc_switch
-On/Off Switch for RC models driven by PWM signal. Based on ATtiny85, 12V capable.
+# ATtiny RC Switch
+
+![3D PCB](res/attiny_rc_switch.png)
+
+Order the PCB [here](https://oshpark.com/shared_projects/kK0Np74q)
+
+On/Off Switch for RC models driven by PWM signal. Based on ATtiny85, 12V capable. Original project is located [here](https://github.com/DzikuVx/attiny_rc_switch).
 
 This small device allows to turn on/off different devices on RC devices: airplanes, quadcopter drones, cars. 
 It uses one RC PWM channel as input and provides power on output pins when channel pulse length is above 1700us.
 Excellent to switch LED strips. Output current is up to 500mA.
 
-# Hardware
+## Hardware
+
+![PCB Top](res/attiny_rc_switch_pcb_top.png)
+![PCB Bottom](res/attiny_rc_switch_pcb_bottom.png)
 
 * AVR ATtiny x5 series microcontroller. Compiled code takes slightly above 2kB, so will fit into ATtiny45 and ATtiny85. When smoothing is removed, code will fit into ATtiny25
-* 470 Ohm resistor
-* BD139-16 or similar NPN bipolar transistor
 
-![diagram](attiny_rc_switch.png)
+### BOM
 
-# Notes
+| Label |Part No    | Name     | Quantity | Subtotal | Link   |
+|-------|-----------|----------|----------|----------|--------|
+| U1 | ATTINY85-20SU | ATtiny85 Microcontroller | 1 | $1.50 | [DigiKey](https://www.digikey.com/en/products/detail/microchip-technology/ATTINY85-20SU/735470) |
+| U2 | IRLML2502TRPBF | N-Channel MOSFET, 20V, 4.2A SOT-23 | 1 | $0.43 | [DigiKey](https://www.digikey.com/en/products/detail/infineon-technologies/IRLML2502TRPBF/812502) |
+| D1 | LTST-C190KRKT | Red LED, SMD 0603, Clear | 1 | $0.14 | [DigiKey](https://www.digikey.com/en/products/detail/liteon/LTST-C190KRKT/386817) |
+| R1,R2 | CRGCQ0603F470R | 470 Ohm Resistor, SMD 0603, 1/10W | 2 | $0.20 | [DigiKey](https://www.digikey.com/en/products/detail/te-connectivity-passive-product/CRGCQ0603F470R/8576286) |
+| C1 | CC0603KRX7R7BB104 | 0.1uF Capacitor, SMD 0603, 16V | 1 | $0.08 | [DigiKey](https://www.digikey.com/en/products/detail/yageo/CC0603KRX7R7BB104/302822) |
+|  |  |  | Total | $2.35 |  |
+
+### Schematic
+
+![diagram](res/attiny_rc_switch_schematic.png)
+
+## Notes
 
 * Arduino IDE 1.6.6. Should work on older releases, but never tested
+
 * ATtiny45/85 board definitions. Tested with [damellis/attiny](https://github.com/damellis/attiny)
+
 * Uses 8MHz internal osciallator 
-* BD139 resistor capabilities are: max current load 1.5A and up to 8W. But, there are few limitations:
-    * base resistor is scaled to allow saturation on up to 400mA
-    * On higher loads voltage drop might appear
-    * On 12V and 400mA power is already on 6W, so really, do not exceed this limit, thing about radiator and so on...
--- attiny_rc_switch.ino
+++ attiny_rc_switch.ino
@@ -1,10 +1,11 @@
+/* Original Code: https://github.com/DzikuVx/attiny_rc_switch */
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <PinChangeInterrupt.h>
 
+#define SLEEP_MODE_PWR_SAVE     (_BV(SM0) | _BV(SM1))
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 #define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
-
 #define CHANNEL_PIN 0
 #define OUTPUT_PIN 3
 #define LPF_FACTOR 0.5
@@ -13,7 +14,6 @@
 volatile unsigned long rising_start = 0;
 volatile long channel_length = 0;
 volatile long channel_raw = 0;
-
 bool state = false;
 
 void enterSleep(void) {
@@ -24,12 +24,11 @@ void enterSleep(void) {
 }
 
 /*
-   Start watchdog timer
-   0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-   6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
-*/
+ * Start watchdog timer
+ * 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+ * 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+ */
 void setup_watchdog(int ii) {
-
   byte bb;
   int ww;
   if (ii > 9 ) ii = 9;
@@ -37,48 +36,37 @@ void setup_watchdog(int ii) {
   if (ii > 7) bb |= (1 << 5);
   bb |= (1 << WDCE);
   ww = bb;
-
   MCUSR &= ~(1 << WDRF);
-  // start timed sequence
-  WDTCR |= (1 << WDCE) | (1 << WDE);
-  // set new watchdog timeout value
-  WDTCR = bb;
+  WDTCR |= (1 << WDCE) | (1 << WDE); // start timed sequence
+  WDTCR = bb; // set new watchdog timeout value
   WDTCR |= _BV(WDIE);
 }
 
 /*
  * Watchdog interrupt function
  * Actually, we can do absolutely nothing here...
-*/
-ISR(WDT_vect) {
-}
+ */
+ISR(WDT_vect) {}
 
 void setup() {
   adc_disable();
-
   pinMode(CHANNEL_PIN, INPUT);
   pinMode(OUTPUT_PIN, OUTPUT);
+  digitalWrite(OUTPUT_PIN, HIGH);
+  delay(3000);
   digitalWrite(OUTPUT_PIN, LOW);
-
   attachPinChangeInterrupt(CHANNEL_PIN, onChange, CHANGE);
-
-  /*
-   * Setup watchdog to wakeup ATTiny every 128ms to enable/diable output
-   */
-  setup_watchdog(2);
+  setup_watchdog(2); //Set watchdog to wakeup every 128ms to enable/diable output
 }
 
 int smooth(int data, float filterVal, float smoothedVal){
-
   if (filterVal > 1){      // check to make sure param's are within range
     filterVal = .99;
-  }
-  else if (filterVal <= 0){
+  } else if (filterVal <= 0){
     filterVal = 0;
   }
 
   smoothedVal = (data * (1 - filterVal)) + (smoothedVal  *  filterVal);
-
   return (int)smoothedVal;
 }
 
@@ -89,22 +77,15 @@ void onChange(void) {
     rising_start = micros();
   } else if (trigger == FALLING) {
     channel_raw = micros() - rising_start;
-
-    /*
-     * Add some smoothing to prevent from input jitter
-     */
-    channel_length =  smooth(channel_raw, LPF_FACTOR, channel_length);
+    channel_length = smooth(channel_raw, LPF_FACTOR, channel_length); //Add smoothing to prevent input jitters
   }
-
 }
 
 void loop() {
-
   if (channel_length > PULSE_THRESHOLD) {
     digitalWrite(OUTPUT_PIN, HIGH);
   } else {
     digitalWrite(OUTPUT_PIN, LOW);
   }
-
   enterSleep();
 }
